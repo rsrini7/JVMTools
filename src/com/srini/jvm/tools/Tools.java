abbc6e6e10d5c5418d607097a99c66035101646e
rsrini7 <srinivas.enfs@gmail.com>
2015-05-01 20:07:52 +0530

    Jautodoc added to test ssh

--- src/com/srini/jvm/tools/Tools.java
+++ src/com/srini/jvm/tools/Tools.java
@@ -2,8 +2,16 @@ package com.srini.jvm.tools;
 
 import com.sun.tools.attach.VirtualMachine;
 
+/**
+ * The Class Tools.
+ */
 public class Tools {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		VirtualMachine.list().forEach(s -> System.out.println(s));
 	}
